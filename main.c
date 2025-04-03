4128ef184c7707c11b7605c1c0d5dc92345a1522
Sebas <psanchez9@milaifontanals.org>
2025-04-03 19:47:11 +0200

    fitxer per a random_c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    // Inicialitzar la llavor de nombres aleatoris amb el temps actual
+    srand(time(NULL));
+    
+    // Generar un nombre aleatori entre 0 i 99
+    int random_number = rand() % 100;
+    
+    // Mostrar el nombre generat
+    printf("Numero aleatori: %d\n", random_number);
+    
+    return 0;
+}
